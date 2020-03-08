ac2b72fbf333ad0b684c1ef5a6c9071c65a140a1
Stefan Scheler <stefan@scheler.com>
2020-03-08 19:37:49 +0100

    version bump

--- src/misc/globals.cpp
+++ src/misc/globals.cpp
@@ -19,7 +19,7 @@
 #include "globals.h"
 
 const QString Globals::ApplicationName = "BossKey";
-const SoftwareVersion Globals::ApplicationVersion = SoftwareVersion("2020.3a");
+const SoftwareVersion Globals::ApplicationVersion = SoftwareVersion("2020.3b");
 const QString Globals::ApplicationFullName = Globals::ApplicationName + " " + Globals::ApplicationVersion.toString();
 const QString Globals::ApplicationWebsite = "https://scheler.com/bosskey/";
 const quint32 Globals::ApplicationRevision = REVISION;
